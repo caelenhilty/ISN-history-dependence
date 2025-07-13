a07aa2942bc775c449a3c329fe33d3be1d166a84
caelenhilty <caelenhilty@gmail.com>
2025-07-13 15:34:48 -0600

    figure 3b data generation

---
+++ figures/figure3b/figure3b.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm 
+import pandas as pd
+
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+from model import util, left_right_task as lrt
+
+Wji, pset, amp, dur, l_kernel, r_kernel = util.load_fiducial_network()
+
+# task parameters
+numPairs = 5
+dt = 1e-5
+seq_len = 6
+sequences = lrt.make_all_sequences(seq_len, ['L', 'R'])
+equil_duration = 2
+
+# now a bunch of trials! -- keep mapping between sequence and decision
+n_trials = 5
+seq_len = len(sequences[0])
+p_curves = np.zeros((n_trials, seq_len + 1))
+reliabilities = np.zeros(n_trials)
+decision_dict = {i:{} for i in range(n_trials)}
+for i in tqdm(range(n_trials)):
+    rng = np.random.default_rng(i)
+    
+    # make kernels -- same mean
+    l_kernel = rng.lognormal(0, 1, numPairs)
+    l_kernel = l_kernel / np.sum(l_kernel) * numPairs
+    l_kernel = np.round(l_kernel, 5)
+    r_kernel = rng.lognormal(0, 1, numPairs)
+    r_kernel = r_kernel / np.sum(r_kernel) * numPairs
+    r_kernel = np.round(r_kernel, 5)
+    
+    # make stimuli
+    stim_map = lrt.make_stim_map(numPairs, amp, dur, l_kernel, r_kernel, dt)
+    
+    # make FSM
+    FSM = lrt.make_FSM(numPairs, pset, Wji, stim_map, 2, dt=dt)
+    
+    reliabilities[i] = lrt.FSM_reliability(sequences, FSM)
+    
+    # pcurve and decision dictionary computations -----------------------
+    # trace all sequences on the graph
+    nodes_dict = {node: [] for node in list(FSM.nodes)}
+    for seq in sequences:
+        # start at the beginning
+        current_node = 1
+        # trace the sequence
+        for letter in seq:
+            for edge in FSM.out_edges(current_node, keys=True):
+                if FSM.edges[edge]['label'] == letter:
+                    # move to the next node
+                    current_node = edge[1]
+                    break
+        nodes_dict[current_node].append(seq)
+
+    left_counts = np.zeros(seq_len + 1)
+    left_choices = np.zeros(seq_len + 1)
+    for node in nodes_dict:
+        # is the node a L or R node? based on frequency
+        l_cue_counts = [seq.count('L') for seq in nodes_dict[node]]
+        num_above = sum([1 for count in l_cue_counts if count > seq_len/2])
+        num_below = sum([1 for count in l_cue_counts if count < seq_len/2])
+        if num_above == num_below:
+            for count in l_cue_counts:
+                left_counts[count] += 1     # record distribution of `counts` terminating in current node
+                left_choices[count] += 0.5   # record how many times each `count` resulted in a left decision
+            for seq in nodes_dict[node]:
+                decision_dict[i][seq] = 'Tie'
+        else:
+            left_decision = num_below < num_above # True if L, False if R
+            for count in l_cue_counts:
+                left_counts[count] += 1     # record distribution of `counts` terminating in current node
+                left_choices[count] += left_decision   # record how many times each `count` resulted in a left decision
+        # update dictionary
+        for seq in nodes_dict[node]:
+            decision_dict[i][seq] = 'L' if left_decision else 'R'
+    
+    p_curves[i] = left_choices/left_counts
+
+df_decisions = pd.DataFrame(decision_dict)
+
+# make a data folder
+data_dir = util.make_data_folder('figures/figure3b')
+
+# save the dataframe
+df_decisions.to_csv(f'{data_dir}/decision_dict.csv')
+np.save(f'{data_dir}/p_curves.npy', p_curves)
+np.save(f'{data_dir}/reliabilities.npy', reliabilities)
