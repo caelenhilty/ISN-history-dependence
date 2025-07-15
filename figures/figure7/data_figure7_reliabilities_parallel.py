7a284b665f22c94d99dd7a6279b1a85d5763598a
caelenhilty <caelenhilty@gmail.com>
2025-07-15 16:06:31 -0600

    new p_curve data + fig7 parallel updates

--- figures/figure7/data_figure7_reliabilities_parallel.py
+++ figures/figure7/data_figure7_reliabilities_parallel.py
@@ -17,13 +17,6 @@ seq_len = 6
 sequences = lrt.make_all_sequences(seq_len, ['L', 'R'])
 equil_duration = 2
 
-# parameter sweep
-amp_range = np.logspace(0, 2, 50)
-dur_range = np.linspace(0, 0.05, 50)
-dur_mesh, amp_mesh = np.meshgrid(dur_range, amp_range) # amp on y-axis, duration on x-axis
-dur_flat, amp_flat = dur_mesh.ravel(), amp_mesh.ravel()
-reliabilities = np.zeros(dur_flat.shape)
-
 def run_model(dur, amp):
     if dur == 0:
         return 0.5  # return a default reliability for zero duration
@@ -37,12 +30,25 @@ def run_model(dur, amp):
     return reliability
 
 if __name__ == '__main__':
+    import time
+    # parameter sweep
+    print("Running parameter sweep...")
+    n = 50
+    amp_range = np.logspace(0, 2, n)
+    dur_range = np.linspace(0, 0.05, n)
+    dur_mesh, amp_mesh = np.meshgrid(dur_range, amp_range) # amp on y-axis, duration on x-axis
+    dur_flat, amp_flat = dur_mesh.ravel(), amp_mesh.ravel()
+    print(f"Running {len(dur_flat)} simulations...")
+    start = time.time()
     with mp.Pool(mp.cpu_count()) as pool:
         reliabilities = np.array(pool.starmap(run_model, zip(dur_flat, amp_flat)))
-
+    print(f"Completed in {time.time() - start:.2f} seconds.")
+    
     # count the number of reliabilities.npy in the figures/figure6 directory
     data_dir = util.make_data_folder('figures/figure7')
     np.save(data_dir + '/reliabilities.npy', np.array(reliabilities))
+    np.save(data_dir + '/dur_mesh.npy', dur_mesh)
+    np.save(data_dir + '/amp_mesh.npy', amp_mesh)
 
     # quick plot of results
     fig, ax = plt.subplots(layout='constrained')
