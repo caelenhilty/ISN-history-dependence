40a689c83e106f6b16ec11f584a2c98ff5997ecb
caelenhilty <caelenhilty@gmail.com>
2025-07-31 00:57:47 -0600

    Wji run data

--- figures/figure8/heterogeneity_vs_reliability.py
+++ figures/figure8/heterogeneity_vs_reliability.py
@@ -13,25 +13,12 @@ Wji, pset, amp, dur, l_kernel, r_kernel = util.load_fiducial_network()
 # calculate means of Wji, ignoring the diagonal
 Wji_means = [np.mean(w[np.eye(w.shape[0], dtype=bool) == False]) for w in Wji]
 
-data_dir = 'figures/figure8/data_1'
-output_dir = util.make_data_folder(data_dir, name='raw_reliabilities')
-CVs = np.logspace(-0.25, 0.25, 3)
-
 # task parameters
 numPairs = 5
 dt = 1e-5
 seq_len = 6
 sequences = lrt.make_all_sequences(seq_len, ['L', 'R'])
 equil_duration = 2
-
-# universal parameter sweep
-n = 20
-amp_min = max(min(pset[-2]/np.max(l_kernel), pset[-2]/np.max(r_kernel)), 1)  # pset[-2] is thetaE -- need to turn E on to get a response
-# amp_min is chosen so that at least one cue could elicit a response
-amp_range = np.logspace(np.log10(amp_min), 2, n)
-dur_range = np.linspace(0, 0.05, n)
-dur_mesh, amp_mesh = np.meshgrid(dur_range, amp_range) # amp on y-axis, duration on x-axis
-dur_flat, amp_flat = dur_mesh.ravel(), amp_mesh.ravel()
 
 def run_model(Wji, dur, amp):
     if dur == 0:
@@ -46,7 +33,19 @@ def run_model(Wji, dur, amp):
     return reliability
 
 if __name__ == "__main__":
+    data_dir = 'figures/figure8/data_1'
+    output_dir = util.make_data_folder(data_dir, name='raw_reliabilities')
+    CVs = [0.25, 1.00, 2.00]
 
+    # universal parameter sweep
+    n = 20
+    amp_min = max(min(pset[-2]/np.max(l_kernel), pset[-2]/np.max(r_kernel)), 1)  # pset[-2] is thetaE -- need to turn E on to get a response
+    # amp_min is chosen so that at least one cue could elicit a response
+    amp_range = np.logspace(np.log10(amp_min), 2, n)
+    dur_range = np.linspace(0, 0.05, n)
+    dur_mesh, amp_mesh = np.meshgrid(dur_range, amp_range) # amp on y-axis, duration on x-axis
+    dur_flat, amp_flat = dur_mesh.ravel(), amp_mesh.ravel()
+    
     def run_sweep(CV, i):
         # make the Wji matrix
         Wji = np.load(data_dir + f'/CV_{CV:.2f}/Wji_{i+1}.npy', allow_pickle=True)
@@ -66,8 +65,11 @@ if __name__ == "__main__":
     n_networks = 10  # number of networks per CV
     for CV in CVs:
         os.makedirs(Path(output_dir + f'/CV_{CV:.2f}'), exist_ok=True)
+        print(f"Running for CV={CV:.2f} with {n_networks} networks...")
         for i in range(n_networks):  # run n_networks for each CV
+            print(f"Running for CV={CV:.2f}, network {i+1}/{n_networks}", end='\r')
             run_sweep(CV, i)
+        print(f"\nFinished running for CV={CV:.2f}")
     
     # also run for CV = 0.00
     os.makedirs(Path(output_dir + '/CV_0.00'), exist_ok=True)
